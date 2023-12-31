9f54d986ef9f4a5d386ea5f8ca3010cec8645470
kobiowuquadri <kobiowuq@gmail.com>
2023-12-31 09:14:04 -0600

    add user authorisation on app.jsx

--- client/src/API/apiCalls.js
+++ client/src/API/apiCalls.js
@@ -1,35 +1,42 @@
 import axios from 'axios'
 
-export const userRegister = async (payload) => {
-   const response = await axios.post('http://localhost:5000/api/v1/user-register', payload, {
-     withCredentials: true
-   })
-   console.log(response)
-   return response
+export const userRegister = async payload => {
+  const response = await axios.post(
+    'http://localhost:5000/api/v1/user-register',
+    payload,
+    {
+      withCredentials: true
+    }
+  )
+  console.log(response)
+  return response
 }
 
-export const userLogin = async (payload) => {
-   const response = await axios.post('http://localhost:5000/api/v1/user-login', payload, {
-     withCredentials: true
-   })
-   //  usertoken
-  //  console.log(response?.data)
-  //  const { token } = response?.data?.isUser
-  //  console.log(token)
-  //  const userToken = localStorage.setItem('userToken', JSON.stringify())
-  //  console.log(response)
-   return response
+export const userLogin = async payload => {
+  const response = await axios.post(
+    'http://localhost:5000/api/v1/user-login',
+    payload,
+    {
+      withCredentials: true
+    }
+  )
+  console.log(response?.data)
+  const { token } = response.data
+  localStorage.setItem('userToken', JSON.stringify({token}))
+  return response
 }
 
 // Submit Application
 
-export const applyForJob = async (payload) => {
-  const response = await axios.post('http://localhost:5000/api/v1/user-apply', payload, {
-    withCredentials: true
-  })
-  return response 
+export const applyForJob = async payload => {
+  const response = await axios.post(
+    'http://localhost:5000/api/v1/user-apply',
+    payload,
+    {
+      withCredentials: true
+    }
+  )
+  return response
 }
 
-
 // Admin API
-
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Home from './Pages/Home/Home'
-import {Routes, Route, Outlet} from 'react-router-dom'
+import {Routes, Route, Outlet, Navigate} from 'react-router-dom'
 import Login from './Pages/Login/Login'
 import Register from './Pages/Register/Register'
 import Dashboard from './Pages/UserDashboard/Dashboard'
@@ -10,6 +10,9 @@ import Apply from './Pages/UserDashboard/Apply'
 
 function App() {
 
+  const isUser = JSON.parse(localStorage.getItem("userToken"));
+  console.log(isUser);
+
   return (
     <>
     <ToastContainer/>
@@ -17,10 +20,10 @@ function App() {
         <Route index element={<Home/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
         <Route path='/register' element={<Register/>}></Route>
-        <Route path='/dashboard' element={<Outlet />}>
+        <Route path='/dashboard' element={isUser ?  <Outlet /> : <Navigate to="/login"/>}>
             <Route index element={<Dashboard/>}></Route>
             <Route path='jobs' element={<ViewJobs />}></Route>
-            <Route path='applyforjob' element={<Apply />}></Route>
+            <Route path='applyforjob' element={isUser ? <Apply /> : <Navigate to='/login'/>}></Route>
           </Route>
       </Routes>
     </>
