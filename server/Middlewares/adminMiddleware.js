355244b33592d3ba3b4c778a502f295a3a28f1bd
kobiowuquadri <kobiowuq@gmail.com>
2024-01-12 06:01:27 -0600

    update user middleware for clearer debugging

--- server/Middlewares/adminMiddleware.js
+++ server/Middlewares/adminMiddleware.js
@@ -2,10 +2,10 @@ const jwt = require('jsonwebtoken')
 
 const authorizedAdmin = (req, res, next) => {
   try {
-    const token = req.headers['authorization'].split(' ')[1]
-    jwt.verify(token, process.env.SECRET, async (err, decodedToken) => {
+    const adminToken = req.headers['authorization'].split(' ')[1]
+    jwt.verify(adminToken, process.env.SECRET, async (err, decodedToken) => {
       if (err) {
-        throw new Error(err)
+        console.log(err)
       }
       req.body.adminId = decodedToken.id
       next()
--- server/Middlewares/userMiddleware.js
+++ server/Middlewares/userMiddleware.js
@@ -1,22 +1,27 @@
-const jwt = require('jsonwebtoken')
-
+const jwt = require('jsonwebtoken');
 
 const authorizedUser = (req, res, next) => {
   try {
-    const token = req.headers['authorization'].split(' ')[1]
+    const token = req.headers['authorization'].split(' ')[1];
+
     jwt.verify(token, process.env.SECRET, async (err, decodedToken) => {
-       if (err){
-        throw new Error(err)
-       }
-       req.body.userId = decodedToken.id
-       next()
-    })
+      if (err) {
+        console.log(err.message);
+        return res.status(401).send({ auth: false, message: 'UnAuthorized.' });
+      }
 
+      if (decodedToken) {
+        req.body.userId = decodedToken.id;
+        next();
+      } else {
+        console.log('Decoded token is undefined');
+        return res.status(401).send({ auth: false, message: 'UnAuthorized.' });
+      }
+    });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(401).send({ auth: false, message: 'UnAuthorized.' });
   }
-  catch(err){
-    console.log(err.message)
-    return res.status(401).send({ auth: false, message: 'UnAuthorized.' })
-  }
-}
+};
 
-module.exports = authorizedUser
+module.exports = authorizedUser;
