534107ae7fac84dcbaaaa46d9029f43e52791fd2
kobiowuquadri <kobiowuq@gmail.com>
2024-01-22 06:59:15 -0600

    add service worker file

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -3,11 +3,9 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
       caches.open('your-cache-name').then((cache) => {
         return cache.addAll([
-          // List of files to cache
           '/',
           '/index.html',
-          '/app.js',
-          // Add more files as needed
+          '/app.jsx',
         ]);
       })
     );
