2decd9fc539a72c7807efc3523de133373d2e7a6
kobiowuquadri <kobiowuq@gmail.com>
2024-01-30 08:41:22 -0600

    remove public/ from the images url

--- client/src/Components/About/About.jsx
+++ client/src/Components/About/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import bgImage from '../../../public/Assets/team_images.jpeg'
+import bgImage from '/Assets/team_images.jpeg'
 import './about.scss'
 import { Link } from 'react-router-dom'
 
--- client/src/Components/Team/Team.jsx
+++ client/src/Components/Team/Team.jsx
@@ -1,45 +1,29 @@
 import React from 'react'
-
-const card = [
-  {
-    image: 'Assets/team2.png',
-    name: 'Quadri Kobiowu',
-    desc: 'A Software Developer'
-  },
-  {
-    image: 'Assets/team1.png',
-    name: 'Simon Azike',
-    desc: 'A project Manager'
-  },
-  {
-    image: 'Assets/team2.png',
-    name: 'Simo Mohammad',
-    desc: 'An PHP developer'
-  },
-  {
-    image: 'Assets/team1.png',
-    name: 'Sodiq Kobiowu',
-    desc: 'Frontend Developer'
-  }
-]
+import teams from '../../Assets/Data/team.json'
 
 function Team () {
   return (
-      <div className='container p-5'>
+    <div className='container p-5'>
       <h1 className='p-4 text-center fw-bold'>Meet Our Team</h1>
-          <div className='row row-cols-1 row-cols-lg-4 pb-3' >
-            {card.map((team, index) => {
-                return (
-                    <div data-aos="flip-left" key={index} className='d-flex justify-content-center shadow p-5 flex-column text-center align-items-center'>
-                        <p><img width={'150px'} src={team.image} alt="my-image" /></p>
-                        <p>{team.name}</p>
-                        <p>{team.desc}</p>
-                        <button className='btn btn-blue text-white'>Connect</button>
-                    </div>
-                )
-            })}
-          </div>
+      <div className='row row-cols-1 row-cols-lg-4 pb-3'>
+        {teams.map((team, index) => {
+          return (
+            <div
+              data-aos='flip-left'
+              key={index}
+              className='d-flex justify-content-center shadow p-5 flex-column text-center align-items-center'
+            >
+              <p>
+                <img width={'150px'} src={team.image} alt='my-image' />
+              </p>
+              <p>{team.name}</p>
+              <p>{team.desc}</p>
+              <button className='btn btn-blue text-white'>Connect</button>
+            </div>
+          )
+        })}
       </div>
+    </div>
   )
 }
 
