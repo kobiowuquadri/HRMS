82a392e61cc08c2c6fc988d7106beb423beffade
kobiowuquadri <kobiowuq@gmail.com>
2024-05-13 15:57:47 -0500

    add: static file middleware

--- server/index.js
+++ server/index.js
@@ -1,39 +1,37 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const connectToDB = require('./Database/db')
-const userRouter = require('./Routes/userRoutes.js')
-const adminRouter = require('./Routes/adminRoutes.js')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const connectToDB = require('./Database/db');
+const userRouter = require('./Routes/userRoutes.js');
+const adminRouter = require('./Routes/adminRoutes.js');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const path = require('path');
-
 
 const port = process.env.PORT
 
 // Middlewares
-app.use(cors({
-    origin: "https://hrms-client-self.vercel.app",
-    credentials: true
-}))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static('public'));
 
-// Home
+// Home route
 app.get('/', (req, res) => {
-    res.send('Connected to Backend Successfully.')
-})
+    res.send('Connected to Backend Successfully.');
+});
+
 // Routes
-app.use('/api/v1', userRouter)
-app.use('/api/v1', adminRouter)
-connectToDB()
+app.use('/api/v1', userRouter);
+app.use('/api/v1', adminRouter);
 
 // Additional middleware to serve static files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+// Connect to database
+connectToDB();
 
-app.listen(port, ()=> {
-    console.log(`Server running on port ${port}`)
-})
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
